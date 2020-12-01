c25469e01af03c03461857a0364ff0e4a097b4d7
Rodrigo <rodrigopba1@gmail.com>
2020-12-01 15:16:46 -0300

    melhorias no script

--- script-temperatura.sh
+++ script-temperatura.sh
@@ -4,19 +4,46 @@
 # Author: Vivek Gite <www.cyberciti.biz> under GPL v2.x+
 # -------------------------------------------------------
 
+touch historico_temperatura.txt
 
+echo "$(date) @ $(hostname)" > historico_temperatura.txt	
+	
 
 while :
 do
+	
 	clear
+	echo ""
+
+
+
 	cpu=$(</sys/class/thermal/thermal_zone0/temp)
+	temp_atual=$((cpu/1000))
+
+	echo $temp_atual >> historico_temperatura.txt
+	
+	
 	echo "$(date) @ $(hostname)"
 	echo "-------------------------------------------"
 	echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
-	echo "CPU => $((cpu/1000))'C"
+	echo "CPU => $((temp_atual))'C"
+
+
+
+
+	echo ""
+	echo "Espaço no dir /"
+	df -h /
+	echo ""
+
 
 	echo "Press [CTRL+C] to stop.."
 	sleep 5
+
+
+
+	
+
 done
 
 
